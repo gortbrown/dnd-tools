877ad4a51a2677fcce655131598a237156e200f3
Garrett Brown <54114771+saladhax@users.noreply.github.com>
2022-01-23 00:05:50 -0600

    Add files via upload

---
+++ dd.js
@@ -0,0 +1,7 @@
+console.log("********D&D Roller********");
+console.log("Options:"));
+console.log("attack(): Choose an attack.");
+console.log("damage(num): You take Damage by num.");
+console.log("heal(num): You heal by num.");
+console.log("hp(): Prints current HP.");
+console.log(
---
+++ dice.js
@@ -0,0 +1,5 @@
+console.log("**********D&D ROLLER**********");
+console.log("Enter command 'd(num)' with num equaling size");
+function d
+	console.log((Math.random() * num) + 1);	
+}
